defdcd405be4e21432897b44d1712c6660724324
JpMayor1 <royamsemaj@gmail.com>
2026-02-24 16:54:22 +0800

    Lesson 1 - C# Basics with Variables and Input. with simple loop

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter you name: ");
+        string? name = Console.ReadLine();
+        while (string.IsNullOrWhiteSpace(name))
+        {
+            Console.WriteLine("Name is required: ");
+            name = Console.ReadLine();
+        }
+
+        int age;
+        while (true)
+        {
+            Console.WriteLine("Enter a valid age: ");
+            string? ageInput = Console.ReadLine();
+            if (int.TryParse(ageInput, out age) && age > 0)
+            {
+                break;
+            }
+        }
+
+
+        Console.WriteLine($"Hi {name}! Your are is {age} year's old.");
+    }
+}
