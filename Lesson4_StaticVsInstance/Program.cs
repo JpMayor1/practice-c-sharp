c01472db94647552aeea56112e5faf2ffd146de3
JpMayor1 <royamsemaj@gmail.com>
2026-02-28 14:38:33 +0800

    Add Lesson 4: Dog class demo with instance methods and multiple objects

---
+++ Lesson4_StaticVsInstance/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Dog
+{
+    public string Name = "";
+
+    public void Bark()
+    {
+        Console.WriteLine($"{Name} says: Woof!");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Dog buddy = new Dog();
+        Dog rocky = new Dog();
+
+        buddy.Name = "Buddy";
+        buddy.Bark();
+
+        rocky.Name = "Rocky";
+        rocky.Bark();
+    }
+}
